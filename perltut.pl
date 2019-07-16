87681c7655f061b200c438fe9512bb18c6917924
unknown <wblake@emsys.net>
2019-07-16 19:48:15 +0100

    More math, assignment, etc.

--- perltut.pl
+++ perltut.pl
@@ -9,13 +9,14 @@ use feature "switch";
 
 use v5.26;
 
-# math
-say " 5 ** 4 = ", 5 ** 4;
+my $rand_num = 5;
+$rand_num += 1 ;
+say "Number Incremented ", $rand_num;
 
-say "EXP 1 = ", exp 1;
-say "HEX 10 = ", hex 10;
-say "OCT 10 = ", oct 10;
-say "INT 6.45 = ", int(6.45);
-say "LOG 2 = ", log 2;
-say "Random between 0 - 10 = " , int(rand 11);
-say "SQRT 9 = " ,sqrt(9);
+say "6++ = ", $rand_num++ ;
+say "++6 = ", ++$rand_num ;
+say "6-- = ", $rand_num--;
+say "--6 = ", --$rand_num ;
+
+say " 3 + 2 * 5 = " , 3 + 2 * 5;
+say " (3 + 2) * 5 = " , (3 + 2) * 5;
