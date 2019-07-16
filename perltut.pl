aa30592271247cf0ecab03fa2fe1c2b482ba8b94
unknown <wblake@emsys.net>
2019-07-16 19:38:13 +0100

    First lesson
    Math Lesson

--- perltut.pl
+++ perltut.pl
@@ -9,37 +9,13 @@ use feature "switch";
 
 use v5.26;
 
-print "Hello World\n";
+# math
+say " 5 ** 4 = ", 5 ** 4;
 
-my $name = 'Wil' ;
-
-my ($age, $street) = (55, '123 Home');
-
-my $my_info = "$name lives on \"$street\"\n";
-
-$my_info = qq{$name lives on "$street" \n};
-
-print $my_info;
-
-my $bunch_on_info = <<"END";
-This is a bunch of information 
-on multiple lines
-END
-
-    say $bunch_on_info ;
-
-    my $big_int = 18446744073709551615;
-
-# %c : Character
-# %s : string
-
-printf("%u \n", $big_int + 1) ;
-
-my $big_float = .1000000000000001;
-
-printf("%.16f \n", $big_float + .1000000000000001 );
-
-my $first = 1;
-my $second = 2;
-($first, $second)  = ($second, $first);
-say "$first $second";
+say "EXP 1 = ", exp 1;
+say "HEX 10 = ", hex 10;
+say "OCT 10 = ", oct 10;
+say "INT 6.45 = ", int(6.45);
+say "LOG 2 = ", log 2;
+say "Random between 0 - 10 = " , int(rand 11);
+say "SQRT 9 = " ,sqrt(9);
