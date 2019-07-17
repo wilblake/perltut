58f5355ed910c74bb47cf164645a900730f5d8ca
unknown <wblake@emsys.net>
2019-07-17 18:39:59 +0100

    Arrays, Strings, Sorting, Grep, Map

--- perltut.pl
+++ perltut.pl
@@ -9,26 +9,53 @@ use feature "switch";
 
 use v5.26;
 
+my @primes = (2,3,5,7,11,13,17);
+my @my_info = ("Wil","123 Market St", 55, 6.25);
+$my_info[4] = "Blake";
+for my $info (@my_info) {
+  say $info;
+}
+foreach my $num (@primes) {
+  say $num;
+}
+for (@my_info) {
+  say $_;
+}
+my @my_name = @my_info[0,4];
+say @my_name ;
 
-my $long_string = "Random Long String";
+my $items = scalar @my_info ;
+say $items ;
+my ($f_name, $address, $how_old, $height, $l_name) = @my_info;
+say "$f_name $l_name" ;
+say "Popped Value ", pop @primes;
+say "Pushed Value "  , push @primes, 17;
+say "First Item ", shift @primes;
+say "Unshifted Item ", unshift @primes,2;
 
-say "Length of string ", length $long_string ;
+# Insert comma after each array member
+print join(", ", @primes), "\n" ;
+say "Remove Index 0 - 2 ", splice @primes,0,3;
 
-printf( "Long is at %d\n", index $long_string, "Long");
-printf ("Last g is at %d\n",rindex $long_string,"g");
+print join " ", ('list', 'of', 'words', "\n");
 
-$long_string = $long_string . ' isn\'t that long' ;
+# String to Array
+  my $customers = "Sue Sally Paul" ;
+  my @cust_array = split / /,$customers;
 
-say "Index 7 through 10 " , substr $long_string,7,4;
+print join(", " , @cust_array),"\n";
 
-$long_string =~ s/ /,/g;
-say $long_string ;
-my $two_times = "What I said is " x 2;
+@cust_array = reverse @cust_array;
+print "Customer Array Reverse ", join(", " , @cust_array),"\n";
 
-say $two_times;
+ @cust_array = reverse sort @cust_array;
+print "Customer Array Reverse Sort ", join(", " , @cust_array),"\n";
 
-my @abcs = ('a' .. 'z') ;
-print join(", ", @abcs, "\n");
+#grep
+my @number_array = (1,2,3,4,5,6,7,8);
+my @odds_array = grep {$_ %2 } @number_array;
+print join(", ", @odds_array), "\n";
 
-  my $letter = 'c';
- say "Next Letter ", ++$letter;
+# map operation on each array element
+my @dbl_array = map{$_ *2} @number_array;
+print join(", ", @dbl_array), "\n";
