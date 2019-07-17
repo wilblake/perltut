852dfafb18d2ede6587dc304a528b19e66d57691
unknown <wblake@emsys.net>
2019-07-17 05:53:23 +0100

    String comparision eq lt gt

--- perltut.pl
+++ perltut.pl
@@ -13,22 +13,13 @@ my $age = 80;
 my $is_not_intoxicated = 1;
 my $age_last_exam  = 16;
 
-if ($age<16) {
-    say "You can't drive" ;
-    
-} elsif (!$is_not_intoxicated) {
-    say "You can't drive" ;
-}
-else { say "You can drive" };
-
-if ( ($age >= 1) && ($age < 16) ) {
-    say "You can't drive" ;
-    
-} elsif (!$is_not_intoxicated) {
-    say "You can't drive" ;
-} elsif (  (($age>=80) && ($age > 100)) || ( ($age - $age_last_exam) > 5)) {
-    say "You can't drive" ;
-    
+if ('a' eq 'b' ){
+    say "a equals b" ;
 } else {
-    say "You can Drive" ;
+    say " a doesn't equal b" ;
 }
+
+unless (!$is_not_intoxicated) {
+    say "Get Sober" ;
+}
+say (( $age>18 ) ? "Can Vote" : "Can't Vote" );
