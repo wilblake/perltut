b51a53c2901775d705955f24791f6a775849fb0a
unknown <wblake@emsys.net>
2019-07-17 06:37:40 +0100

    More Strings

--- perltut.pl
+++ perltut.pl
@@ -10,14 +10,25 @@ use feature "switch";
 use v5.26;
 
 
-my $i = 1;
+my $long_string = "Random Long String";
 
-my $age_old = 17;
-given($age_old) {
-    when($_ > 16 ){
-	say "Drive" ;
-    continue ;
-}
-when ($_ > 17 ) {say "Go Vote";}
-default { say "Nothing Special" ;}
-}
+say "Length of string ", length $long_string ;
+
+printf( "Long is at %d\n", index $long_string, "Long");
+printf ("Last g is at %d\n",rindex $long_string,"g");
+
+$long_string = $long_string . 'isn\'t that long' ;
+
+say "Index 7 through 10 " , substr $long_string,7,4;
+
+my $animal = "animals";
+
+printf("Last character is %s\n", chop $animal);
+
+
+my $no_newline = "No Newline\n" ;
+
+    chomp $no_newline ;
+    printf("no_newline %s", $no_newline);
+
+print("Uppercase: %s \n", uc $long_string);
